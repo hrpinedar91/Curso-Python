19a71c4e5b59f356db7bfa5d8f0bec38d0185f86
Harold Pineda <hrpinedar91@gmail.com>
2022-01-03 08:19:54 -0500

    add folder operator aritmeticos

---
+++ Exercises/Exercice02.py
@@ -0,0 +1,11 @@
+payaso = 112
+muneca = 75
+
+orderPayaso = 23 
+orderMuneca = 54
+
+pesoTotal = ((orderPayaso * payaso) + orderMuneca * muneca)
+
+print("el peso total del pedido es: " , pesoTotal)
+
+
